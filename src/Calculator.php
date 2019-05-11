e56f2197905e0dd1dd461b309a81d9fc5594bcdc
Rimas Kudelis <rq@akl.lt>
2019-05-11 10:12:27 +0300

    calculateForSinglePayment() now accepts $daysAfterFirstAdvance as float

--- src/Calculator.php
+++ src/Calculator.php
@@ -59,7 +59,7 @@ class Calculator
         return new static($firstAdvance);
     }
 
-    public function calculateForSinglePayment(float $payment, int $daysAfterAdvance, int $round = self::DEFAULT_PRECISION): float
+    public function calculateForSinglePayment(float $payment, float $daysAfterAdvance, int $round = self::DEFAULT_PRECISION): float
     {
         return round(
             (
