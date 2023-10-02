e539ac5c4a79dd447bcb673ad694ee9fdd33131e
Rimas Kudelis <rimas@kudelis.lt>
2023-10-02 11:16:22 +0300

    Change remaining GPL references to MIT

--- src/Calculator.php
+++ src/Calculator.php
@@ -5,23 +5,12 @@ namespace RQ\APRCalculator;
 use DomainException;
 
 /**
- * This file is part of APR Calculator.
- *
- * APR Calculator is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * APR Calculator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * See the GNU General Public License for more details <http://www.gnu.org/licenses/>.
- *
  * @copyright © 2014 Stephen Haunts
  * @copyright © 2019 Rimas Kudelis
  * @author Stephen Haunts http://www.stephenhaunts.com
  * @author Graham Johnson
  * @author Rimas Kudelis https://rimas.kudelis.lt
- * @license http://www.gnu.org/licenses/ GNU General Public License, version 2 or later
+ * @license MIT
  * @link https://github.com/rimas-kudelis/php-apr-calculator
  */
 class Calculator
